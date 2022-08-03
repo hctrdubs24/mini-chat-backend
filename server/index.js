be49cb7589504e4cb1ccb020b7025142cf0ba484
hctrdubs24 <OtracuentadeHector24@outlook.com>
2022-08-03 09:37:07 -0500

    init2

---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import morgan from "morgan";
+import { Server as SocketServer } from "socket.io";
+import http from "http";
+import cors from "cors";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const server = http.createServer(app);
+const io = new SocketServer(server, {
+  cors: {
+    origin: "*",
+  },
+});
+// En el caso de origin de puede agregar un '*' para indicar que se puede conectar cualquiera
+
+// midlewares
+app.use(cors());
+app.use(morgan("dev"));
+
+io.on("connection", (socket) => {
+  console.log(`User ${socket.id} connected`);
+  socket.on("message", (message) => {
+    console.log(message);
+    socket.broadcast.emit("message", {
+      body: message,
+      from: socket.id,
+    });
+  });
+});
+
+app.use(express.static(join(__dirname, "../client/build")));
+
+server.listen(process.env.PORT || 4000, () =>
+  console.log(`Server listening on port ${process.env.PORT || 4000}`)
+);
